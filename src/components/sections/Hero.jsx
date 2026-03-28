77a41d42ab1bf675c14f36ce6545d9afa14969da
Jorge Matos <jisapjisap@gmail.com>
2026-03-28 18:11:30 +0100

    feat: add Hero section component and reusable MainBtn UI component

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -8,6 +8,7 @@ import "swiper/css/effect-fade"
 import slide1 from "/images/Index/Hero/main-slider-01.jpg"
 import slide2 from "/images/Index/Hero/main-slider-02.jpg"
 import slide3 from "/images/Index/Hero/main-slider-03.jpg"
+import MainBtn from "../ui/Buttons/MainBtn"
 
 
 
@@ -31,7 +32,22 @@ const Hero = () => {
               className="px-[4%] md:px-[8%] xl:px-[12%] py-[8%] xl:py-[12%] min-h-screen w-full flex flex-col justify-center items-center bg-no-repeat bg-cover bg-center"
               style={{ backgroundImage: `url(${slide1})` }}
             >
+              <div className="hero-content text-white text-center">
+                <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold pb-5">
+                  Transform Your <span className="block text-coffee w-fit mx-auto">Espace</span>
+                </h1>
 
+                <p className="max-w-4xl mx-auto pb-14 lg:pb-18 text-gray-50 font-light text-md lg:text-lg">
+                  Bring your vision to life with sophisticated interiors that combine style, comfort, and functionality, creating a home that truly reflects your personality and taste.
+
+                </p>
+
+                <MainBtn
+                  text={"Explore Designs"}
+                  path="/about"
+                  className="w-45!"
+                />
+              </div>
 
             </div>
           </SwiperSlide>
