96d1bf137c33e5164466968d6850999c98d8960b
Jorge Matos <jisapjisap@gmail.com>
2026-03-28 15:42:58 +0100

    feat: add AuthModal component and implement responsive MobileMenu navigation

--- src/components/layout/Navbar/MobileMenu.jsx
+++ src/components/layout/Navbar/MobileMenu.jsx
@@ -62,23 +62,58 @@ const MobileMenu = ({ menuOpen, setMenuOpen, navLinks }) => {
 
               {item.submenu && (
                 <div className={`overflow-hidden transition-all duration-500
-                    ${activeMenu === index ? "max-h-60-mt-4" : "max-h-0"}`}
+                    ${activeMenu === index ? "max-h-60 mt-4" : "max-h-0"}`}
                 >
-                  <div className="flex flex-col gap-3">
-                    <NavLink
-                      key={i}
-                      to={sub.path}
-                      onClick={() => setMenuOpen(false)}
-                      className="text-gray-300 hover:text-white"
-                    >
-                      {sub.name}
-                    </NavLink>
-                  </div>
+                  {item.submenu.map((sub, i) => (
+                    <div key={i} className="flex flex-col gap-3">
+                      <NavLink
+                        key={i}
+                        to={sub.path}
+                        onClick={() => setMenuOpen(false)}
+                        className="text-gray-300 hover:text-white"
+                      >
+                        {sub.name}
+                      </NavLink>
+                    </div>
+                  ))}
                 </div>
               )}
             </div>
-
           ))}
+        </div>
+
+        <hr className="mt-8 text-gray-50/20" />
+
+        <ul className="mx-auto flex items-center justify-center py-5 gap-3">
+          <li>
+            <Link to="/" className="p-3 rounded-full bg-linear-to-r from-purple-500 to-pink-500 transition-all duration-300 rotate-hover inline-block">
+              <Facebook className="text-white" size={20} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="p-3 rounded-full bg-linear-to-r from-purple-500 to-pink-500 transition-all duration-300 rotate-hover inline-block">
+              <Instagram className="text-white" size={20} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="p-3 rounded-full bg-linear-to-r from-purple-500 to-pink-500 transition-all duration-300 rotate-hover inline-block">
+              <Twitter className="text-white" size={20} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="p-3 rounded-full bg-linear-to-r from-purple-500 to-pink-500 transition-all duration-300 rotate-hover inline-block">
+              <Dribbble className="text-white" size={20} />
+            </Link>
+          </li>
+        </ul>
+
+        <div className="absolute bottom-5 left-0 w-full text-center space-y-4">
+          <p className="text-sm text-gray-400 flex items-center justify-center gap-1">
+            <Copyright size={16} />
+            <span>
+              {year} Lumor. All Rights Reserved
+            </span>
+          </p>
         </div>
       </div>
     </>
