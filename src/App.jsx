4c480648f4b0474f632e36ee5d5532301098d492
Jorge Matos <jisapjisap@gmail.com>
2026-03-30 16:26:06 +0200

    feat: implement GSAP ScrollSmoother and add Shop component structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,34 @@ import Navbar from './components/layout/Navbar/Navbar'
 import Footer from './components/layout/Footer/Footer'
 import Home from './pages/Home'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import gsap from 'gsap'
 import ScrollSmoother from 'gsap/ScrollSmoother'
+import gsap from 'gsap'
 
 
 const App = () => {
 
-  const contactRef = useRef(null);
+  const smootherWrapperRef = useRef(null);
+  const smootherContentRef = useRef(null);
 
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-    const smoother = ScrollSmoother.create({
-      smooth: 1.8,
-      effects: true,
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);  // Plugin de GSAP para scroll suave
+    const smoother = ScrollSmoother.create({             // Creación del scroll suave
+      wrapper: smootherWrapperRef.current,
+      content: smootherContentRef.current,
+      smooth: 1.8,                                       // Suavidad del scroll
+      effects: true,                                     // Efectos de scroll
     });
     return () => {
-      smoother && smoother.kill();
-      ScrollTrigger.getAll().forEach(t => t.kill());
+      smoother && smoother.kill();                       // Limpieza del scroll suave
+      ScrollTrigger.getAll().forEach(t => t.kill());     // Limpieza de los triggers
     }
   }, [])
 
   return (
     <>
-      <div id="smooth-wrapper" ref={contactRef}>
+      <div id="smooth-wrapper" ref={smootherWrapperRef}>
         <Navbar />
-        <div id="smooth-content">
+        <div id="smooth-content" ref={smootherContentRef}>
           <div className='min-h-screen overflow-clip'>
             <Routes>
               <Route path='/' element={<Home />} />
