5f799d804ce766be39bdd658d6e2322767894374
Jorge Matos <jisapjisap@gmail.com>
2026-03-30 16:16:38 +0200

    feat: initialize App component with GSAP ScrollSmoother integration and routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar/Navbar'
 
 import Footer from './components/layout/Footer/Footer'
 import Home from './pages/Home'
-
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import gsap from 'gsap'
+import ScrollSmoother from 'gsap/ScrollSmoother'
 
 
 const App = () => {
+
+  const contactRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+    const smoother = ScrollSmoother.create({
+      smooth: 1.8,
+      effects: true,
+    });
+    return () => {
+      smoother && smoother.kill();
+      ScrollTrigger.getAll().forEach(t => t.kill());
+    }
+  }, [])
+
   return (
     <>
-      <div id="smooth-wrapper">
+      <div id="smooth-wrapper" ref={contactRef}>
         <Navbar />
         <div id="smooth-content">
           <div className='min-h-screen overflow-clip'>
